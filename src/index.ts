40e2cb9dd497bf87329ce5e305795ce4d6f0a36c
Ruchir <ruchirkhare12@gmail.com>
2024-06-23 13:49:09 +0530

    db connect

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log('Hello, TS finally!');
+import express from 'express';
+import bodyParser from 'body-parser';
+import http from 'http';
+import cookieParser from 'cookie-parser';   
+import cors from 'cors';
+import compression from 'compression';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+
+
+const app = express();
+
+app.use(cors({ credentials: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+app.use(morgan('dev'));
+
+
+const server = http.createServer(app);
+
+server.listen(8080,()=>{
+    console.log('Server is running on port 8080');  
+})
+
+
+const MONGO_URI = 'mongodb+srv://ruchirkhare12:oEg8TS54njUgvqIT@cluster0.fs775mq.mongodb.net/API?retryWrites=true&w=majority&appName=Cluster0';  // mongodb connection string
+
+
+mongoose.connect(MONGO_URI)
+.then(()=>{
+    console.log('Connected to MongoDB');
+})
+.catch((err)=>{
+    console.log('Error:',err);
+})
+
+
+
+
+
